f133e25e77de20a407713678857a4d546b7913d3
fblondel <fblondel@FBLONDEL-SURFAC>
2021-12-01 20:34:45 -0600

    Add project files.

---
+++ Days/Day1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021.Days
+{
+    public static class Day1Part1
+    {
+        public static int Run()
+        {
+            int[] measures = Array.ConvertAll(File.ReadAllLines(@"Inputs\Day1.txt"), int.Parse);
+            var total = 0;
+
+            for (int i = 0; i < measures.Length - 1; i++)
+            {
+                if (measures[i] < measures[i + 1])
+                    total++;
+            }
+
+            return total;
+        }
+    }
+
+    public static class Day1Part2
+    {
+        public static int Run()
+        {
+            int[] measures = Array.ConvertAll(File.ReadAllLines(@"Inputs\Day1.txt"), int.Parse);
+            var total = 0;
+
+            for (int i = 0; i < measures.Length - 3; i++)
+            {
+                if ((measures[i] + measures[i + 1] + measures[i + 2]) < (measures[i + 1] + measures[i + 2] + measures[i + 3]))
+                    total++;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode2021.Days;
+using static System.Console;
+
+namespace AdventOfCode2021
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("+ * = * + = * = +| Advent of Code 2021 |+ = * = + * = * +");
+            WriteLine("                 =>+-+>   Day 1   <+-+<=                 ");
+            WriteLine("           >-=>>+-+>   Sonar Sweep   <+-+<<=-<           ");
+            WriteLine($"+> Part 1: {Day1Part1.Run()} measurements are larger than the previous measurement");
+            WriteLine($"+> Part 2: {Day1Part2.Run()} sums are larger than the previous sum");
+        }
+    }
+}
