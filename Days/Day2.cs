b801103a7625357f03a82dc69e24096fe6d85408
Francois Blondel <fblondel@vivaldicap.com>
2021-12-02 12:28:25 -0600

    Day 2

---
+++ Days/Day2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021.Days
+{
+    public static class Day2Part1
+    {
+        public static int Run()
+        {
+            var commands = Array.ConvertAll(File.ReadAllLines(@"Inputs\Day2.txt"), ToTuple);
+            int hPos = 0, depth = 0;
+
+            foreach(var command in commands)
+            {
+                switch (command.Action)
+                {
+                    case "forward":
+                        hPos += command.Value;
+                        break;
+                    case "down":
+                        depth += command.Value;
+                        break;
+                    case "up":
+                        depth -= command.Value;
+                        break;
+                }
+            }
+
+            return hPos * depth;
+        }
+
+        public static (string Action, int Value) ToTuple(string command) => command.Split(' ') switch { var chunks => (chunks[0], int.Parse(chunks[1])) };
+    }
+
+    public static class Day2Part2
+    {
+        public static int Run()
+        {
+            var commands = Array.ConvertAll(File.ReadAllLines(@"Inputs\Day2.txt"), ToTuple);
+            int hPos = 0, depth = 0, aim = 0;
+
+            foreach (var command in commands)
+            {
+                switch (command.Action)
+                {
+                    case "forward":
+                        hPos += command.Value;
+                        depth += aim * command.Value;
+                        break;
+                    case "down":
+                        aim += command.Value;
+                        break;
+                    case "up":
+                        aim -= command.Value;
+                        break;
+                }
+            }
+
+            return hPos * depth;
+        }
+
+        public static (string Action, int Value) ToTuple(string command) => command.Split(' ') switch { var chunks => (chunks[0], int.Parse(chunks[1])) };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,10 +8,14 @@ namespace AdventOfCode2021
         public static void Main(string[] args)
         {
             WriteLine("+ * = * + = * = +| Advent of Code 2021 |+ = * = + * = * +");
-            WriteLine("                 =>+-+>   Day 1   <+-+<=                 ");
-            WriteLine("           >-=>>+-+>   Sonar Sweep   <+-+<<=-<           ");
-            WriteLine($"+> Part 1: {Day1Part1.Run()} measurements are larger than the previous measurement");
-            WriteLine($"+> Part 2: {Day1Part2.Run()} sums are larger than the previous sum");
+            //WriteLine("                 =>+-+>   Day 1   <+-+<=                 ");
+            //WriteLine("           >-=>>+-+>   Sonar Sweep   <+-+<<=-<           ");
+            //WriteLine($"+> Part 1: {Day1Part1.Run()} measurements are larger than the previous measurement");
+            //WriteLine($"+> Part 2: {Day1Part2.Run()} sums are larger than the previous sum");
+            WriteLine("                 =>+-+>   Day 2   <+-+<=                 ");
+            WriteLine("              >-=>>+-+>   Dive!   <+-+<<=-<              ");
+            WriteLine($"+> Part 1: Final Horizontal Position * Final Depth = {Day2Part1.Run()}");
+            WriteLine($"+> Part 2: Final Horizontal Position * Final Depth = {Day2Part2.Run()}");
         }
     }
 }
