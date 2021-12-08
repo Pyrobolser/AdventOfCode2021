0402e69c455ac595633ec3ecb2619ec81532ab26
Francois Blondel <fblondel@vivaldicap.com>
2021-12-08 17:48:32 -0600

    Day8Part1!

---
+++ Days/Day8.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Days
+{
+    public static class Day8Part1
+    {
+        public static int Run()
+        {
+            var digits = Array.ConvertAll(File.ReadAllLines(@"Inputs\Day8.txt"), GetDigits);
+
+            return digits.Sum();
+        }
+
+        public static int GetDigits(string line)
+        {
+            var digits = line.Split(" | ")[1].Split(' ').Where(s => s.Count() == 2 || s.Count() == 3 || s.Count() == 4 || s.Count() == 7).Count();
+
+            return digits;
+        }
+    }
+
+    public static class Day8Part2
+    {
+        public static int Run()
+        {
+            var inputs = Array.ConvertAll(File.ReadAllLines(@"Inputs\Day8.txt"), GetInputs);
+            var total = 0;
+
+            //foreach(var input in inputs)
+            //{
+            //    if(input.Digits.Where(s => s.Count() == 2 || s.Count() == 3 || s.Count() == 4 || s.Count() == 7).Count() == 4)
+            //    {
+            //        total += GetValue(input.Digits);
+            //    }
+            //}
+
+            return total;
+        }
+
+        public static (string[] Signals, string[] Digits) GetInputs(string line)
+        {
+            var values = line.Split(" | ").Select(s => s.Split(' ').Select(e => string.Concat(e.OrderBy(e => e))).ToArray()).ToArray();
+
+            return (Signals: values[0], Digits: values[1]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -32,10 +32,15 @@ namespace AdventOfCode2021
             //WriteLine("           >-=>>+-+>   Lanternfish   <+-+<<=-<           ");
             //WriteLine($"+> Part 1: There are {Day6Part1.Run()} lanternfish after 80 days");
             //WriteLine($"+> Part 2: There are {Day6Part2.Run()} lanternfish after 256 days");
-            WriteLine("                 =>+-+>   Day 7   <+-+<=                 ");
-            WriteLine("     >-=>>+-+>   The Treachery of Whales   <+-+<<=-<     ");
-            WriteLine($"+> Part 1: They must spend {Day7Part1.Run()} fuel to align to that position");
-            WriteLine($"+> Part 2: They must spend {Day7Part2.Run()} fuel to align to that position");
+            //WriteLine("                 =>+-+>   Day 7   <+-+<=                 ");
+            //WriteLine("     >-=>>+-+>   The Treachery of Whales   <+-+<<=-<     ");
+            //WriteLine($"+> Part 1: They must spend {Day7Part1.Run()} fuel to align to that position");
+            //WriteLine($"+> Part 2: They must spend {Day7Part2.Run()} fuel to align to that position");
+            WriteLine("                 =>+-+>   Day 8   <+-+<=                 ");
+            WriteLine("      >-=>>+-+>   Seven Segment Search   <+-+<<=-<       ");
+            WriteLine($"+> Part 1: In the output values, 1, 4, ,7 or 8 appear {Day8Part1.Run()} times");
+            WriteLine($"+> Part 2: The sum of all the output values is {Day8Part2.Run()}");
+
         }
     }
 }
